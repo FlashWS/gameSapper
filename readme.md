ac4d1137b05a0b4f892bc063c8ec495992fc4828
Лисин Сергей <sergey@abpgroup.ru>
2018-05-15 13:44:01 +0300

    Рефакторинг. Отметка где находится бомба.

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Игра "Саппер"
+
+SVG+JavaScript
+
+ссылка на демо https://flashws.github.io/gameTag/ 
--- sapper.js
+++ sapper.js
@@ -4,39 +4,33 @@
 
 "use strict";
 
-let sapperCSS = document.createElement("link");
-sapperCSS.rel = "stylesheet";
-sapperCSS.href = "sapper.css";
-document.head.insertBefore(sapperCSS, document.head.childNodes[document.head.childNodes.length - 1].nextSibling);
+document.head.insertAdjacentHTML('beforeEnd', '<link rel="stylesheet" href="sapper.css">');
 
 class Sapper {
-  constructor () {
+  constructor (width = 16, height = 16, mines = 40, sizeField = 20) {
     this.defaultValues = {
-      width: 16,
-      height: 16,
-      mines: 40,
+      width: width,
+      height: height,
+      mines: mines,
     };
-    this.cells = [];
-    this.sizeField = 20;
+
+    this.sizeField = sizeField;
+
+    this.cells = [
+      ...Array(this.defaultValues.mines).fill(true),
+      ...Array(this.defaultValues.width * this.defaultValues.height - this.defaultValues.mines).fill(false)
+    ];
   }
 
   run () {
-    this._getRandomizeValues();
+    this._randomizeValues();
     this._setHelpNumbers();
     this._drawGameField();
   }
 
-  _countCells () {
-    return this.defaultValues.width * this.defaultValues.height;
-  }
-
-  _getRandomizeValues () {
+  _randomizeValues () {
     let x = 0;
     let y = 0;
-
-    for (let i = 0; i < this._countCells(); i++) {
-      this.cells.push((i < this.defaultValues.mines));
-    }
 
     this.cells = this.cells
       .sort(() => Math.random() - 0.5)
@@ -58,25 +52,56 @@ class Sapper {
 
   _setHelpNumbers () {
     this.cells.forEach(cell => {
-      if (cell.mine === false) {
-        let counter = 0;
-        for (let x = -1; x <= 1; x++) {
-          for (let y = -1; y <= 1; y++) {
-            if (y !== 0 || x !== 0) {
-              let find = this.cells.filter(cellAround => cellAround.mine === true)
-                .filter(cellAround => cellAround.x === cell.x + x)
-                .filter(cellAround => cellAround.y === cell.y + y);
-              if (find.length === 1) {
-                counter++;
-              }
-            }
-          }
-        }
-        if (counter > 0) {
-          cell.hint = counter;
-        }
+      if (cell.mine === true) {
+        return;
+      }
+
+      let counter = this._aroundCells(this._findMine)(cell.x, cell.y);
+
+      if (counter > 0) {
+        cell.hint = counter;
       }
     });
+  }
+
+  _aroundCells (f) {
+    let counter = 0;
+
+    return (x, y) => {
+      for (let lX = -1; lX <= 1; lX++) {
+        for (let lY = -1; lY <= 1; lY++) {
+          if (lY !== 0 || lX !== 0) {
+            counter += f.apply(this, [x, y, lX, lY]);
+          }
+        }
+      }
+      return counter;
+    }
+  }
+
+  _findMine (x, y, lX, lY) {
+    let find = this.cells.filter(cellAround => cellAround.mine === true)
+      .filter(cellAround => cellAround.x === x + lX)
+      .filter(cellAround => cellAround.y === y + lY);
+    return find.length;
+  }
+
+  _openEmptyCell (x, y, lX, lY) {
+    let find = this.cells.filter(cellAround => cellAround.mine === false)
+      .filter(cellAround => cellAround.x === lX + x)
+      .filter(cellAround => cellAround.y === lY + y);
+
+    find.forEach(cell => {
+      if (cell.status === 'open') {
+        return;
+      } else {
+        cell.status = 'open';
+      }
+      if (cell.hint) {
+        return;
+      }
+      this._aroundCells(this._openEmptyCell)(cell.x, cell.y);
+    })
   }
 
   _drawGameField () {
@@ -87,37 +112,44 @@ class Sapper {
 
     this.cells.forEach((item, i) => {
       let g = '';
-      if(item.status === 'close'){
+      if (item.status === 'close') {
         g = `
-        <g data-id="${i}" class="cell">
-            <rect
-              class="cell-close"
-              x="${item.x * this.sizeField}"
-              y="${item.y * this.sizeField}"
-              width="${this.sizeField}"
-              height="${this.sizeField}">
-            </rect>
-        </g>
-      `;
+              <g data-id="${i}" class="cell">
+                  <rect
+                    class="cell-close"
+                    x="${item.x * this.sizeField}"
+                    y="${item.y * this.sizeField}"
+                    width="${this.sizeField}"
+                    height="${this.sizeField}">
+                  </rect>
+                  <text
+                    class="cell-text mark"
+                    text-anchor="middle"
+                    x="${item.x * this.sizeField + this.sizeField / 2}"
+                    y="${item.y * this.sizeField + this.sizeField / 2 + 5}">
+                      ${(item.mark) ? 'b' : ''}
+                  </text>
+              </g>
+            `;
       } else {
         g = `
-        <g data-id="${i}" class="cell">
-            <rect
-              class="cell-rect ${(item.mine) ? 'cell-mine' : 'card-square-empty'}"
-              x="${item.x * this.sizeField}"
-              y="${item.y * this.sizeField}"
-              width="${this.sizeField}"
-              height="${this.sizeField}">
-            </rect>
-            <text
-              class="cell-text ${(item.mine) ? 'mine' : ''}"
-              text-anchor="middle"
-              x="${item.x * this.sizeField + this.sizeField / 2}"
-              y="${item.y * this.sizeField + this.sizeField / 2 + 5}">
-                ${(item.mine) ? 'x' : (item.hint || '')}
-            </text>
-        </g>
-      `;
+              <g data-id="${i}" class="cell">
+                  <rect
+                    class="cell-rect ${(item.mine) ? 'cell-mine' : 'card-square-empty'}"
+                    x="${item.x * this.sizeField}"
+                    y="${item.y * this.sizeField}"
+                    width="${this.sizeField}"
+                    height="${this.sizeField}">
+                  </rect>
+                  <text
+                    class="cell-text ${(item.mine) ? 'mine' : ''}"
+                    text-anchor="middle"
+                    x="${item.x * this.sizeField + this.sizeField / 2}"
+                    y="${item.y * this.sizeField + this.sizeField / 2 + 5}">
+                      ${(item.mine) ? 'x' : (item.hint || '')}
+                  </text>
+              </g>
+            `;
       }
       html += g;
     });
@@ -134,55 +166,44 @@ class Sapper {
 
   _clickCell (id) {
     let cell = this.cells[id];
-    if (cell.status === 'open') {
+
+    if (cell.status === 'open' || cell.mark === true) {
       return false;
-    } else if (cell.mine === true) {
+    }
+
+    if (cell.mine === true) {
       this._gameOver();
     } else if (cell.hint === undefined && cell.mine === false) {
-      this._openEmptyCells(cell.x, cell.y);
-      this._drawGameField();
+      this._aroundCells(this._openEmptyCell)(cell.x, cell.y);
     } else {
       cell.status = 'open';
-      this._drawGameField();
     }
+
+    this._drawGameField();
   }
 
   _labelCell (id) {
     let cell = this.cells[id];
-    console.log('Клик правой клавишей' + cell);
-    return false;
-  }
 
-  _openEmptyCells (x, y) {
-    for (let lX = -1; lX <= 1; lX++) {
-      for (let lY = -1; lY <= 1; lY++) {
-        if (lY !== 0 || lX !== 0) {
-          let find = this.cells.filter(cellAround => cellAround.mine === false)
-            .filter(cellAround => cellAround.x === lX + x)
-            .filter(cellAround => cellAround.y === lY + y);
-          find.forEach( cell => {
-            if(cell.status === 'open'){
-              return;
-            } else {
-              cell.status = 'open';
-            }
-            if(cell.hint){
-              return;
-            }
-            this._openEmptyCells(cell.x, cell.y);
-          })
-
-        }
-      }
+    if (cell.status === 'open') {
+      return false;
     }
+
+    if (cell.mark === true) {
+      delete cell.mark;
+    } else {
+      cell.mark = true;
+    }
+
+    this._drawGameField();
+
+    return false;
   }
 
   _gameOver () {
     this.cells.forEach(cell => {
       cell.status = 'open';
     });
-
-    this._drawGameField();
   }
 }
 
